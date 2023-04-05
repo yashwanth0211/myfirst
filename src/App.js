940a4a4802d7ace5c75485fa336500e3aca58be1
yashwanth0211 <reddy.yashwanthnalla@gmail.com>
2023-04-05 00:32:32 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// FUnctional Component
+//
+const App = () => {
+  let name = "john";
+  return (
+    <div>
+      <center>
+        <h3> Welcome {name} </h3>
+        <h3> Welcome {name} </h3>
+        <h3> Welcome {name} </h3>
+        <h3> Welcome {name} </h3>
+      </center>
+    </div>
+  );
+};
+
+export default App;
